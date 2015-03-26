9cc9208c3e2e4d3d6b359f9cda0d85eb562a9c7e
Sivamuthu Kumar <Ksivamuthu@ceiamerica.com>
2015-03-25 20:05:02 -0400

    Initial Commit

---
+++ Dialer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Dialer
+//
+//  Created by Sivamuthu Kumar on 3/25/15.
+//  Copyright (c) 2015 Vatsalya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Dialer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Dialer
+//
+//  Created by Sivamuthu Kumar on 3/25/15.
+//  Copyright (c) 2015 Vatsalya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
